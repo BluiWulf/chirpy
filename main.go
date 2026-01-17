f1599b125358272b3b7475df15efd51c87ea8820
BluiWulf <bluiwulf@gmail.com>
2026-01-17 12:40:49 -0500

    Starting chirpy project for http server in go course from boot.dev

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{}
+
+	server.Handler = mux
+	server.Addr = ":8080"
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("error starting server: %v", err)
+	}
+}
